7d7622e4bb966fc187c5f7c389a7b86a7691e542
Erick Lima <elima@elconsulting.com.br>
2025-05-10 14:34:28 +0000

    Deploy src_customer model

---
+++ models/src/src_customer.sql
@@ -0,0 +1,14 @@
+{% do log("Criacao do SRC_CUSTOMER", info=True)%}
+
+select
+    c_custkey,
+    c_name,
+    c_address,
+    c_nationkey,
+    c_phone,
+    c_acctbal,
+    c_mktsegment,
+    c_comment
+from {{ source('snowflake_sample', 'raw_customer') }}
+
+{% do log("Criacao do SRC_CUSTOMER concluida", info=True)%}
